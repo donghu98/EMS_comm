5c031154b8440e9f0ee0bf0b0a0322f549792a99
김동후 <tomlmot@naver.com>
2022-06-27 12:30:54 +0900

    2022.06.27

---
+++ windows/gpio_pyqt_main10.py
@@ -0,0 +1,65 @@
+## LED Control UI
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import RPi.GPIO as GPIO
+import time
+
+BUTTON = 3
+RED = 11
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(RED, GPIO.OUT)
+GPIO.setup(BUTTON, GPIO.IN)
+
+class MyApp(QWidget) :
+    def __init__(self) :
+        super().__init__()
+        self.initUI()
+
+    def initUI(self) :
+        self.setWindowTitle('RPi LED Control')
+        # 윈도우 기본 설정
+        self.setGeometry(100, 100, 300, 350)
+        self.label = QLabel(self)
+        self.label.setFont(QFont('Arial', 15))
+        self.label.setText("LED Control")
+        self.label.setAlignment(Qt.AlignCenter) # 라벨 정중앙
+        self.btnOn = QPushButton('LED ON', self)
+        self.btnOff = QPushButton('LED OFF', self)
+
+        # 시그널 정의
+        self.btnOn.clicked.connect(self.btnOn_Clicked)
+        self.btnOff.clicked.connect(self.btnOff_Clicked)
+        
+        self.vbox = QVBoxLayout(self)
+        self.vbox.addWidget(self.label)
+        
+        self.hbox = QHBoxLayout(self)
+        self.hbox.addWidget(self.btnOn)
+        self.hbox.addWidget(self.btnOff)
+
+        self.vbox.addLayout(self.hbox)
+
+        self.show()
+
+    def btnOn_Clicked(self) :
+        self.label.setText('LED ON')
+        GPIO.output(RED, GPIO.HIGH)
+    def btnOff_Clicked(self) :
+        self.label.setText('LED OFF')
+        GPIO.output(RED, GPIO.LOW)
+
+    def closeEvent(self, QCloseEvent) :
+        GPIO.output(RED, GPIO.low)
+        GPIO.cleanup()
+
+        self.deleteLater()
+        QCloseEvent.accept()
+
+if __name__ == '__main__' :
+    app = QApplication(sys.argv)
+    wnd = MyApp()
+
+    app.exec_()        
